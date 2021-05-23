b53ccd597fc3c3920a1ac2ee0b0a5013799b9bed
Tangguh Tri Prasetyo <tangguhtriprasetyo@gmail.com>
2021-05-24 04:42:56 +0700

    Instrumented Test Done

--- app/src/androidTest/java/com/example/dicodingmovieapps/ui/home/HomeActivityTest.kt
+++ app/src/androidTest/java/com/example/dicodingmovieapps/ui/home/HomeActivityTest.kt
@@ -1,30 +1,48 @@
 package com.example.dicodingmovieapps.ui.home
 
 import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.example.dicodingmovieapps.R
-import com.example.dicodingmovieapps.utils.DummyData
+import com.example.dicodingmovieapps.utils.EspressoIdlingResource
 import org.hamcrest.Matchers.not
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 class HomeActivityTest {
 
-    private val dummyMovies = DummyData.generateDataMovies()
-
     @get:Rule
     var activityRule = ActivityScenarioRule(HomeActivity::class.java)
+
+    @Before
+    fun setUp() {
+        ActivityScenario.launch(HomeActivity::class.java)
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.idlingResource)
+    }
+
+    @After
+    fun tearDown() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.idlingResource)
+    }
 
     @Test
     fun loadListMoviesAndTvSeries() {
         onView(withId(R.id.rv_movies)).check(matches(isDisplayed()))
-        onView(withId(R.id.rv_movies)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyMovies.size))
+        onView(withId(R.id.rv_movies)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                10
+            )
+        )
         onView(withId(R.id.rv_movies)).check(matches(isDisplayed()))
 
         onView(withId(R.id.tv_error_message)).check(matches(not(isDisplayed())))
@@ -33,7 +51,12 @@ class HomeActivityTest {
 
     @Test
     fun loadDetailMoviesAndTvSeries() {
-        onView(withId(R.id.rv_movies)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(withId(R.id.rv_movies)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
 
         onView(withId(R.id.img_header_detail)).check(matches(isDisplayed()))
         onView(withId(R.id.img_cast1)).check(matches(isDisplayed()))
@@ -43,14 +66,14 @@ class HomeActivityTest {
         onView(withId(R.id.ic_duration)).check(matches(isDisplayed()))
         onView(withId(R.id.toolbar_detail)).check(matches(isDisplayed()))
         onView(withId(R.id.collapse_toolbar)).check(matches(isDisplayed()))
-
-        onView(withId(R.id.tv_release_date)).check(matches(withText(dummyMovies[0].releaseDate)))
-        onView(withId(R.id.tv_duration)).check(matches(withText(dummyMovies[0].duration)))
-        onView(withId(R.id.tv_genre)).check(matches(withText(dummyMovies[0].genre)))
-        onView(withId(R.id.tv_overview)).check(matches(withText(dummyMovies[0].overview)))
+        onView(withId(R.id.tv_release_date)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_duration)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_genre)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_overview)).check(matches(isDisplayed()))
 
         onView(withId(R.id.tv_error_message)).check(matches(not(isDisplayed())))
         onView(withId(R.id.progress_bar)).check(matches(not(isDisplayed())))
+
         pressBack()
     }
 }
